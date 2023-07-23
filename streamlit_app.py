0b68cd8c417b9b29762c28de0ae5b551c0cdcf23
kranth kumar jorrigala <39906073+kranthikumar9625@users.noreply.github.com>
2023-07-23 16:23:35 +0530

    Update streamlit_app.py
    added titles & menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
+
+streamlit.header('Breakfast Meanu')
+streamlit.text('Omega 3  & Blueberry oatmeal')
+streamlit.text('Kale, Spinach & Rocket smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
